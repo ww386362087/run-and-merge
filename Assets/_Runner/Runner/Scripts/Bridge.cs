f2c7070abe3e3356ab3c3133133ba8d4cb98c9e5
QuangHuyDo <quang.do@city.ac.uk>
2023-02-28 18:06:05 +0700

    working on bridge

--- Assets/_Runner/Runner/Scripts/Bridge.cs
+++ Assets/_Runner/Runner/Scripts/Bridge.cs
@@ -14,6 +14,7 @@ namespace HyperCasual.Runner
 
         [SerializeField] float lengthBridge;
         [SerializeField] float duration =1f;
+        [SerializeField] float lengthScaleFactor = 13.89f;
 
         public float Length
         {
@@ -26,14 +27,13 @@ namespace HyperCasual.Runner
         protected override void Awake()
         {
             base.Awake();
-            bridge.transform.localScale = Vector3.zero;
+            //bridge.transform.localScale = Vector3.zero;
         }
 
-        [ContextMenu("Reset")]
         public override void ResetSpawnable()
         {
             base.ResetSpawnable();
-            bridge.transform.localScale = Vector3.zero;
+            //bridge.transform.localScale = Vector3.zero;
         }
 
         public void Active()
@@ -41,14 +41,38 @@ namespace HyperCasual.Runner
             ScaleBridge();
         }
 
-        [ContextMenu("Scale Bridge")]
         void ScaleBridge()
         {
-            DOVirtual.Float(0, Length, duration, value => {
+            Transform ground = bridge.transform.GetChild(0);
+            Transform handRail = bridge.transform.GetChild(1);
+            Transform left_front_pillar = bridge.transform.GetChild(2);
+            Transform left_back_pillar = bridge.transform.GetChild(3);
+            Transform right_front_pillar = bridge.transform.GetChild(4);
+            Transform right_back_pillar = bridge.transform.GetChild(2);
+
+            float scaleFactor = Length / lengthScaleFactor;
+
+            left_front_pillar.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            left_back_pillar.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            right_front_pillar.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            right_back_pillar.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            DOVirtual.Float(0, scaleFactor, duration, value => {
                 var targetScale = value;
 
-                bridge.transform.localScale = new Vector3(scale.x, scale.y, targetScale);
-                bridge.transform.localPosition = new Vector3(0, -.5f, targetScale/2);
+                //bridge.transform.localScale = new Vector3(scale.x, scale.y, targetScale);
+                //bridge.transform.localPosition = new Vector3(0, -.5f, targetScale/2);
+
+                ground.localScale = new Vector3(scale.x, scale.y, targetScale);
+                ground.localPosition = new Vector3(0, -0.3f, targetScale / 2);
+
+                handRail.localScale = new Vector3(scale.x, scale.y, targetScale);
+                handRail.localPosition = new Vector3(0, -0.3f, targetScale / 2);
+
+                
             });
         }
 

c58dbe3e4951effd2b240cae4671285698591f6a
NguyenDuyThanh1998 <112918496+NguyenDuyThanh1998@users.noreply.github.com>
2023-03-06 18:56:47 +0700

    add adsfreebutton

--- Assets/_Runner/Shared/Scripts/UI/AdsFreeView.cs
+++ Assets/_Runner/Shared/Scripts/UI/AdsFreeView.cs
@@ -12,6 +12,30 @@ namespace HyperCasual.Runner
         [SerializeField]
         HyperCasualButton m_ButtonClose;
         [SerializeField]
-        HyperCasualButton m_PayToRemoveAds;
+        HyperCasualButton m_ButtonRemoveAds;
+
+        private void OnEnable()
+        {
+            m_ButtonClose.AddListener(OnButtonCloseClick);
+            m_ButtonRemoveAds.AddListener(OnButtonRemoveAdsClick);
+        }
+
+        private void OnDisable()
+        {
+            m_ButtonClose.RemoveListener(OnButtonCloseClick);
+            m_ButtonRemoveAds.RemoveListener(OnButtonRemoveAdsClick);
+        }
+
+        void OnButtonCloseClick()
+        {
+            UIManager.Instance.ClearHistory();
+            m_CloseViewEvent.Raise();
+            UIManager.Instance.Show<Hud>();
+        }
+
+        void OnButtonRemoveAdsClick()
+        {
+            // FOR MAD. DO STH HERE
+        }
     }
 }
